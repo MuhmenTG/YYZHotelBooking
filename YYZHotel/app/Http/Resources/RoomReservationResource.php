6d828ce8d0cf577770a07bc89426215300155291
MuhmenTG <mpe@nota.dk>
2023-07-14 00:01:19 +0200

    feat (RoomReservationResource) add resource to format response

---
+++ YYZHotel/app/Http/Resources/RoomReservationResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Factories\BookingFactory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RoomReservationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'identifer' => $this->id,
+            'confirmationNumber' => $this->confirmationNumber,
+            'bookingDate' => $this->bookingDate,
+            'headGuestName' => $this->headGuest,
+            'headGuestemail' => $this->email,
+            'headGuestcontact' => $this->contact,
+            'assignedBookedRoom' =>  $this->roomId,
+            'scheduledCheckInDate' => $this->ScheduledCheckInDate,
+            'scheduledCheckOutDate' => $this->ScheduledCheckOutDate,
+            'actualCheckInDate' => $this->ActualCheckInDate,
+            'actualCheckOutDate' => $this->ActualCheckOutDate,
+            'guests' => $this->guests,
+            'specialRequests' => $this->specialRequests,
+            'bookingStatus' => $this->isConfirmed,
+            'paymentConfirmationNumber' => $this->paymentId,
+        ];
+    }
+}
