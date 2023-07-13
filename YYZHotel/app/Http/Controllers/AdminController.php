3b920d73306d306cfc1344e6f5dd0704c40085a9
MuhmenTG <mpe@nota.dk>
2023-07-14 00:00:05 +0200

    feat (adminContrroller) add CI/CO functionality, room logging and search bookings.

--- YYZHotel/app/Http/Controllers/AdminController.php
+++ YYZHotel/app/Http/Controllers/AdminController.php
@@ -6,8 +6,12 @@ use App\Factories\BookingFactory;
 use App\Helper\Constants;
 use App\Http\Resources\RomResource;
 use App\Http\Resources\RoomCategoryResource;
+use App\Http\Resources\RoomReservationResource;
 use App\Models\Room;
 use App\Models\RoomCategory;
+use App\Models\RoomHistory;
+use App\Models\RoomReservation;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
@@ -234,25 +238,98 @@ class AdminController extends Controller
 
 
 
-    public function checkInGuest(Request $request){
+    public function checkInGuest(string $confirmationNumber){
+        $reservation = BookingFactory::lookUpRoomReservation($confirmationNumber);
+        if(!$reservation && $reservation == null){
+            return response()->json(['message' => Constants::ROOM_RESERVATION_NOT_FOUND_MESSAGE], Response::HTTP_NOT_FOUND);
+        }
+        
+        $currentDate = Carbon::now(); 
+        
+        if($currentDate < $reservation->getScheduledCheckInDate()){
+            return response()->json(['message' => 'The guest can not check in before scheduled check in date'], Response::HTTP_METHOD_NOT_ALLOWED);
+        }
+
+
+        $reservation = new RoomReservationResource($reservation);
+
+        $reservation->setActualCheckInDate($currentDate);
+        $reservation->save();
+
+        return response()->json(['CheckedIn' => $reservation], Response::HTTP_OK);
 
     }
 
     
-    public function checkOutGuest(Request $request){
+    public function checkOutGuest(string $confirmationNumber){
+        $reservation = BookingFactory::lookUpRoomReservation($confirmationNumber);
+        if(!$reservation && $reservation == null){
+            return response()->json(['message' => Constants::ROOM_RESERVATION_NOT_FOUND_MESSAGE], Response::HTTP_NOT_FOUND);
+        }
+
+        if (empty($reservation->getActualCheckInDate())) {
+            return response()->json(['message' => 'The guest is not checked in yet and can not be checked out there for'], Response::HTTP_METHOD_NOT_ALLOWED);
+        }
+        
+        $reservation->setActualCheckOutDate(Carbon::now());
+        $reservation->save();
+
+        $logRoomHistory = new RoomHistory();
+        $logRoomHistory->setRoomId($reservation->getRoomId());
+        $logRoomHistory->setCheckInDate($reservation->getActualCheckInDate());
+        $logRoomHistory->setCheckInDate($reservation->getActualCheckOutDate());
+        $logRoomHistory->setGuestNumber($reservation->getGuests());
+        $logRoomHistory->save();
+
+        $reservation = new RoomReservationResource($reservation);
+
+        return response()->json(['CheckedOut' => $reservation], Response::HTTP_OK);
 
     }
 
     public function getAllCheckedInOutGuests(){
+        $checkedInGuests = RoomReservation::whereNotNull(RoomReservation::COL_ACTUALCHECKINDATE)
+        ->where(RoomReservation::COL_ACTUALCHECKINDATE, '!=', '')
+        ->get();
 
+        $checkedOutGuests = RoomReservation::whereNotNull(RoomReservation::COL_ACTUALCHECKOUTDATE)
+        ->where(RoomReservation::COL_ACTUALCHECKOUTDATE, '!=', '')
+        ->get();
+
+        if ($checkedInGuests->isEmpty() && $checkedOutGuests->isEmpty()) {
+            return response()->json(['message' => 'There are no checked-in or checked-out guests.'], Response::HTTP_METHOD_NOT_ALLOWED);
+        } 
+
+        $checkedInGuests = RoomReservationResource::collection($checkedInGuests);
+        $checkedOutGuests = RoomReservationResource::collection($checkedOutGuests);
+
+        $data = [
+            'checkedInGuests' => $checkedInGuests,
+            'checkedOutGuests' => $checkedOutGuests,
+        ];
+
+        return response()->json(['CheckedOutAndOut' => $data], Response::HTTP_OK);
+        
     }
     
     public function getUpcomingGuestBookings() {
+        $upcomingBookings = RoomReservation::whereNull(RoomReservation::COL_ACTUALCHECKINDATE)
+        ->whereNull(RoomReservation::COL_ACTUALCHECKOUTDATE)
+        ->get();
 
+        if ($upcomingBookings->isEmpty()) {
+            return response()->json(['message' => 'There are no upcoming booking.'], Response::HTTP_NOT_FOUND);
+        } 
+
+        $upcomingBookings = RoomReservationResource::collection($upcomingBookings);
+
+        return response()->json(['upcomingBookings' => $upcomingBookings], Response::HTTP_OK);
+        
     }
 
 
     public function getAllOccipiedBookedRooms() {
+
 
     }
 
@@ -276,20 +353,95 @@ class AdminController extends Controller
 
     }
 
-    public function getRoomLogHistory(){
-        
+    public function getRoomLogHistory(Request $request){
+        $validator = Validator::make($request->all(), [
+            'roomId' => 'nullable|numeric'
+        ]);
+    
+        if ($validator->fails()) {
+            return Constants::validationErrorResponse($validator->errors());
+        }
+    
+        $roomId = $request->input('roomId');
+    
+        if ($roomId) {
+            $room = BookingFactory::lookUpRoom($roomId);
+    
+            if (!$room) {
+                return response()->json(['message' => Constants::ROOM_NOT_FOUND_MESSAGE], Response::HTTP_NOT_FOUND);
+            }
+    
+            $roomHistory = RoomHistory::where(RoomHistory::COL_ROOMID, '=', $roomId)->first();
+    
+            if (!$roomHistory) {
+                return response()->json(['message' => 'There are no recorded room logs with that ID.'], Response::HTTP_NOT_FOUND);
+            }
+    
+            return response()->json(['roomHistoryLog' => $roomHistory], Response::HTTP_OK);
+        }
+    
+        $allRoomHistory = RoomHistory::all();
+    
+        if ($allRoomHistory->isEmpty()) {
+            return response()->json(['message' => 'There are no recorded room logs.'], Response::HTTP_NOT_FOUND);
+        }
+    
+        return response()->json(['roomHistoryLog' => $allRoomHistory], Response::HTTP_OK);
+            
     }
 
     public function updateHotelInfo(){
         
     }
 
-    public function searchBookingsBetweenTwoBookingDates(){
+    public function searchBookingsBetweenTwoBookingDates(Request $request){
+        $validator = Validator::make($request->all(), [
+            'startDate' => 'required|date',
+            'endDate'   => 'required|date|after_or_equal:startDate',
+        ]);
+    
+        if ($validator->fails()) {
+            return Constants::validationErrorResponse($validator->errors());
+        }
+    
+        $startDate = Carbon::parse($request->input('startDate'));
+        $endDate = Carbon::parse($request->input('endDate'));
+    
+        $bookings = RoomReservation::whereBetween(RoomReservation::COL_BOOKINGDATE, [$startDate, $endDate])->get();
 
+        if(!$bookings){
+            return response()->json(['message' => 'There are no bookings found between the dates.'], Response::HTTP_NOT_FOUND);
+        }
+    
+        return response()->json(['bookings' => $bookings], Response::HTTP_OK);
+    
     }
 
-    public function searchBookingsBetweenCheckInDateCheckOutDate(){
+    public function searchBookingsBetweenCheckInDateCheckOutDate(Request $request){
+        $validator = Validator::make($request->all(), [
+            'startDate' => 'required|date',
+            'endDate'   => 'required|date|after_or_equal:startDate',
+        ]);
+    
+        if ($validator->fails()) {
+            return Constants::validationErrorResponse($validator->errors());
+        }
+    
+        $startDate = Carbon::parse($request->input('startDate'));
+        $endDate = Carbon::parse($request->input('endDate'));
+    
+        $bookings = RoomReservation::where(function ($query) use ($startDate, $endDate) {
+            $query->whereBetween(RoomReservation::COL_SCHEDULEDCHECKINDATE, [$startDate, $endDate])
+                ->orWhereBetween(RoomReservation::COL_SCHEDULEDCHECKOUTDATE, [$startDate, $endDate]);
+        })->get();
 
+        if(!$bookings){
+            return response()->json(['message' => 'There are no bookings found between the dates.'], Response::HTTP_NOT_FOUND);
+        }
+
+        $bookings = RoomReservationResource::collection($bookings);
+    
+        return response()->json(['bookings' => $bookings], Response::HTTP_OK);
     }
 
     
