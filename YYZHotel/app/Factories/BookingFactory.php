36b08d012b73c2648838df189e18938932f97542
MuhmenTG <mpe@nota.dk>
2023-07-19 21:54:55 +0200

    feat (BookingFactory) add Stripe payment integration and payment table.

--- YYZHotel/app/Factories/BookingFactory.php
+++ YYZHotel/app/Factories/BookingFactory.php
@@ -2,9 +2,11 @@
 
 namespace App\Factories;
 
+use App\Models\Payment;
 use App\Models\Room;
 use App\Models\RoomReservation;
 use Carbon\Carbon;
+use Stripe\BalanceTransaction;
 
 class BookingFactory {
 
@@ -129,6 +131,72 @@ class BookingFactory {
         $room = Room::ById($roomId)->first();
         return $room;
     }
+
+    public function createCharge(int $amount, string $currency, string $cardNumber, string $expireYear, string $expireMonth, string $cvc, string $confirmationNumber) 
+    {
+        
+        $stripe = BookingFactory::createCardRecord($cardNumber, $expireYear, $expireMonth, $cvc);
+        
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
+
+        $charge = $stripe->charges->create([
+            'amount' => $amount * 100,
+            'currency' => 'dkk',
+            'source' => 'tok_mastercard',
+            'description' => $confirmationNumber,
+        ]);
+        
+        
+        if($charge){    
+            $payment = New Payment();
+            $payment->setPaymentAmount($confirmationNumber);       
+            $payment->setPaymentCurrency($currency);
+            $payment->setPaymentType("Online");
+            $payment->setPaymentTransactionId($charge->id);
+            $payment->setPaymentMethod("MasterCard");
+            $payment->setPaymentGatewayProcessor("Stripe APi");
+            $payment->setPaymentNoteComments($confirmationNumber);
+            $payment->setConfirmationNumber($$amount);
+            $payment->save();
+        }
+        
+        $payment = Payment::ByPaymentTransactionId($charge->id)->get();;
+        return $payment;
+    }
+
+    private function createCardRecord(string $cardNumber, string $expYear, string $expMonth, string $cvc){
+
+        if (!ctype_digit($cardNumber) || strlen($cardNumber) < 12 || strlen($cardNumber) > 19) {
+            throw new \InvalidArgumentException('Invalid card numer given Should be 12 digits long.');
+        }
+
+        if (!ctype_digit($expMonth) || $expMonth < 1 || $expMonth > 12) {
+
+            throw new \InvalidArgumentException('Invalid expiry Date of card.');
+        }
+        
+        if (!ctype_digit($expYear) || strlen($expYear) != 4 || $expYear < date('Y')) {
+    
+        }
+        
+        if (!ctype_digit($cvc) || strlen($cvc) < 3 || strlen($cvc) > 4) {
+        
+        }
+      
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
+        
+        $stripe->tokens->create([
+            'card' => [
+              'number' => $cardNumber,
+              'exp_month' => $expMonth,
+              'exp_year' => $expYear,
+              'cvc' => $cvc,
+            ],
+        ]);
+
+        return $stripe;
+    }
+
 
 
     private static function generateConfirmationNumber(): string
