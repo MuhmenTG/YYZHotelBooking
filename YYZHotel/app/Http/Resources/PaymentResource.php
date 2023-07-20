5cbbb016a433f4dd8c2d6070651cfc5f5f9a7599
MuhmenTG <mpe@nota.dk>
2023-07-20 23:15:46 +0200

    feat (PaymentResource) add resource to format data

---
+++ YYZHotel/app/Http/Resources/PaymentResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PaymentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'paymentAmount' => $this->paymentAmount,
+            'paymentCurrency' => $this->paymentCurrency,
+            'paymentType' => $this->paymentType,
+            'paymentMethod' => $this->paymentMethod,
+            'paymentGatewayProcessor' => $this->paymentGatewayProcessor,
+            'paymentStatus' => $this->paymentStatus,
+            'paymentNoteComments' => $this->paymentNoteComments,
+            'confirmationNumber' => $this->confirmationNumber,
+        ];
+    }
+}
