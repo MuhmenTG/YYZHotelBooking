b0f7ec24edd80364ab4dc97f54c8b979910d2ae5
MuhmenTG <mpe@nota.dk>
2023-07-20 23:14:39 +0200

    feat (constant add new constants

--- YYZHotel/app/Helper/Constants.php
+++ YYZHotel/app/Helper/Constants.php
@@ -11,6 +11,9 @@ class Constants {
     public const ROOM_RESERVATION_DELETION_FAILED_MESSAGE = 'An error occoured. Room reservation could not deleted.'; 
     public const ROOM_NOT_FOUND_MESSAGE = 'The requested room could not be found.';
     public const ROOM_CATEGORY_NOT_FOUND_MESSAGE = 'The requested room category could not be found.';
+    public const NOT_AVALIABLE_ROOMS_ON_REQUESTED_DATES = 'Sorry, but we could not find any rooms on that date or is sold out. Try some other dates.';
+    public const BOOKING_COULD_NOT_BE_DONE = 'Something went wrong under the booking process.';
+
 
     public static function validationErrorResponse($errors): JsonResponse
     {
